66dd798b28293b8b2ddea4b8e35cb7d225cb8a0c
nikitinarthur@gmail.com <U0kBW]icG>
2020-09-19 16:56:57 +0200

    Convert data class to Kotlin

--- app/src/main/java/org/narussia/justfortoday/data/Dairy.kt
+++ app/src/main/java/org/narussia/justfortoday/data/Dairy.kt
@@ -1,70 +1,12 @@
-package org.narussia.justfortoday.data;
+package org.narussia.justfortoday.data
 
-public class Dairy {
-    private String date;
-    private int day;
-    private int month;
-    private String title;
-    private String baseText;
-    private String dayText;
-    private String justForToday;
+class Dairy {
+    var date: String? = null
+    var day = 0
+    var month = 0
+    var title: String? = null
+    var baseText: String? = null
+    var dayText: String? = null
+    var justForToday: String? = null
 
-    public Dairy() {
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBaseText() {
-        return baseText;
-    }
-
-    public void setBaseText(String baseText) {
-        this.baseText = baseText;
-    }
-
-    public String getDayText() {
-        return dayText;
-    }
-
-    public void setDayText(String dayText) {
-        this.dayText = dayText;
-    }
-
-    public String getJustForToday() {
-        return justForToday;
-    }
-
-    public void setJustForToday(String justForToday) {
-        this.justForToday = justForToday;
-    }
 }
