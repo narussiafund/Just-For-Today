52dff069ae540bb93f4b7cdb62822f840ce7515c
nikitinarthur@gmail.com <U0kBW]icG>
2020-09-20 16:36:27 +0200

    Realize get remote diary

--- app/src/main/java/org/narussia/justfortoday/MainActivity.kt
+++ app/src/main/java/org/narussia/justfortoday/MainActivity.kt
@@ -1,7 +1,7 @@
 package org.narussia.justfortoday
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/org/narussia/justfortoday/data/source/dairyRepository/DairyRepository.kt
+++ app/src/main/java/org/narussia/justfortoday/data/source/dairyRepository/DairyRepository.kt
@@ -1,9 +1,34 @@
 package org.narussia.justfortoday.data.source.dairyRepository
 
+import com.google.gson.Gson
 import org.narussia.justfortoday.data.Dairy
+import java.net.URL
+import java.util.*
 
 class DairyRepository : IDairyRepository {
+
+    private val dairyUrl = "https://na-russia.org/egednevnik/dairy_json.php"
+    private val cal = Calendar.getInstance()
+
     override fun getDairy(): Dairy {
+
+        val gson = Gson()
+
+        val thread = Thread {
+            try {
+                val dairyJSON = URL(getDairyUrl()).readText()
+                val dairy: Dairy = gson.fromJson(dairyJSON, Dairy::class.java)
+            } catch (e: Exception) {
+                println("Exception: $e")
+            }
+        }
+        thread.start()
         return Dairy()
     }
+
+    fun getDairyUrl(): String? {
+        return (dairyUrl + "?c=android&d=" + cal.get(Calendar.DAY_OF_MONTH)
+                + "&m=" + (cal.get(Calendar.MONTH) + 1))
+    }
 }
+
