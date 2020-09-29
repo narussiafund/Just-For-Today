acc878d81e4dba8b8b04d9be165bb47730f31b81
nikitinarthur@gmail.com <U0kBW]icG>
2020-09-29 21:04:32 +0200

    Add Retrofit

---
+++ app/src/main/java/org/narussia/justfortoday/network/dairyBuilder.kt
@@ -0,0 +1,21 @@
+package org.narussia.justfortoday.network
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object dairyBuilder {
+
+
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://na-russia.org/egednevnik/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun <T> buildService(service: Class<T>): T {
+        return retrofit.create(service)
+    }
+}
