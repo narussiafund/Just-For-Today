e272c1de90a1447cca6b54efadaa0762da9a9179
Aleksandr Marmyshev <a.marmyshev@seraphim.online>
2020-10-02 18:43:10 +0300

    @refactor retrofit

---
+++ app/src/main/java/org/narussia/justfortoday/data/source/dairyRepository/remote/RemoteDairyRepository.kt
@@ -0,0 +1,38 @@
+package org.narussia.justfortoday.data.source.dairyRepository.remote
+
+import android.util.Log
+import org.narussia.justfortoday.data.Dairy
+import org.narussia.justfortoday.network.IDairyAPI
+import org.narussia.justfortoday.network.NetworkService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.Calendar
+
+class RemoteDairyRepository : IRemoteDairyRepository {
+
+    private val networkService by lazy { NetworkService.instance.buildService(IDairyAPI::class.java) }
+
+    override fun getDairy(callback: (Dairy) -> Unit) {
+        val cal = Calendar.getInstance()
+        networkService.requestDairy(
+            platform = PLATFORM,
+            dayOfMonth = cal.get(Calendar.DAY_OF_MONTH),
+            month = cal.get(Calendar.MONTH) + 1
+        ).enqueue(object : Callback<Dairy> {
+            override fun onResponse(call: Call<Dairy>, response: Response<Dairy>) {
+                response.body()?.let { callback(it) }
+            }
+
+            override fun onFailure(call: Call<Dairy>, t: Throwable) {
+                Log.e("Error get dairy", "Error: ${t.message}")
+            }
+        })
+    }
+
+    companion object {
+        private const val PLATFORM = "android"
+    }
+
+
+}
---
+++ app/src/main/java/org/narussia/justfortoday/network/IDairyAPI.kt
@@ -0,0 +1,25 @@
+package org.narussia.justfortoday.network
+
+import org.narussia.justfortoday.data.Dairy
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface IDairyAPI {
+
+    @GET(DAIRY_JSON)
+    fun requestDairy(
+        @Query(PLATFORM) platform: String,
+        @Query(DAY) dayOfMonth: Int,
+        @Query(MONTH) month: Int,
+    ): Call<Dairy>
+
+    companion object {
+        private const val DAIRY_JSON = "dairy_json.php"
+        private const val PLATFORM = "c"
+        private const val DAY = "d"
+        private const val MONTH = "m"
+    }
+}
+
+
