b4e335ad097ffbc4cfdd979ece72063fabc06c65
nikitinarthur@gmail.com <U0kBW]icG>
2020-10-24 18:47:04 +0200

    @Room fixes 2 lint

--- app/src/main/java/org/narussia/justfortoday/data/Dairy.kt
+++ app/src/main/java/org/narussia/justfortoday/data/Dairy.kt
@@ -3,15 +3,22 @@ package org.narussia.justfortoday.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.*
 
 @Entity
 class Dairy {
-    @PrimaryKey @ColumnInfo(name = "date") var date: String = "01.01"
-    @ColumnInfo(name = "day") var  day = 0
-    @ColumnInfo(name = "month") var  month = 0
-    @ColumnInfo(name = "title") var  title: String = ""
-    @ColumnInfo(name = "basetext") var  basetext: String = ""
-    @ColumnInfo(name = "daytext") var  daytext: String = ""
-    @ColumnInfo(name = "justfortoday") var  justfortoday: String = ""
+    @PrimaryKey
+    @ColumnInfo(name = "date")
+    var date: String = "01.01"
+    @ColumnInfo(name = "day")
+    var day = 0
+    @ColumnInfo(name = "month")
+    var month = 0
+    @ColumnInfo(name = "title")
+    var title: String = ""
+    @ColumnInfo(name = "basetext")
+    var basetext: String = ""
+    @ColumnInfo(name = "daytext")
+    var daytext: String = ""
+    @ColumnInfo(name = "justfortoday")
+    var justfortoday: String = ""
 }
