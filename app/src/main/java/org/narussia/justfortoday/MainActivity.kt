eb9327cd095971906596e76e4425a595e802e532
nikitinarthur@gmail.com <U0kBW]icG>
2020-11-13 16:02:15 +0100

    @Crashlytics add button Crash!

--- app/src/main/java/org/narussia/justfortoday/MainActivity.kt
+++ app/src/main/java/org/narussia/justfortoday/MainActivity.kt
@@ -1,6 +1,8 @@
 package org.narussia.justfortoday
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val crashButton = Button(this)
+        crashButton.text = "Crash!"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
     }
 }
