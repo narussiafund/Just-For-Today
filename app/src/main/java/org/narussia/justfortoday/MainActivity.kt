7daa0edfe0b58f8ba9a88e47eec058f53a629112
nikitinarthur@gmail.com <U0kBW]icG>
2020-11-13 17:37:08 +0100

    @Crashlytics del Crush! button

--- app/src/main/java/org/narussia/justfortoday/MainActivity.kt
+++ app/src/main/java/org/narussia/justfortoday/MainActivity.kt
@@ -1,8 +1,6 @@
 package org.narussia.justfortoday
 
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val crashButton = Button(this)
-        crashButton.text = "Crash!"
-        crashButton.setOnClickListener {
-            throw RuntimeException("Test Crash") // Force a crash
-        }
-
-        addContentView(crashButton, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
     }
 }
