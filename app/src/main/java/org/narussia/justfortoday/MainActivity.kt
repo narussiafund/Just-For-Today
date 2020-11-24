6acbb37f444269d6535c804a604774a4138fc72d
nikitinarthur@gmail.com <U0kBW]icG>
2020-11-24 15:53:37 +0100

    @Hilt fixes for ktLint

--- app/src/main/java/org/narussia/justfortoday/MainActivity.kt
+++ app/src/main/java/org/narussia/justfortoday/MainActivity.kt
@@ -2,7 +2,6 @@ package org.narussia.justfortoday
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
 
 class MainActivity : AppCompatActivity() {
 
